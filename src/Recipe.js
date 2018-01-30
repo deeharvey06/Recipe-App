3f043e8746fd84ab345e2f721dc75af5ac5a5f10
Clifford <deeharvey06@yahoo.com>
2018-01-30 13:50:39 -0800

    new Recipe form

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,28 +1,39 @@
-import React , { Component } from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import './Recipe.css';
 
 class Recipe extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    ingredients: PropTypes.arrayOf(PropTypes.string).isRequired,
+    instructions: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    onDelete: PropTypes.func.isRequired
+  }
+  
   render() {
-    const {title, img, instrusction} = this.props
-    const ingredients = this.props.ingredients.map((ing, index) => ( 
-      <li key={index}>{ing}</li>
+    const {title, img, instructions, id, onDelete} = this.props;
+    const ingredients = this.props.ingredients.map((ing, index) => (
+      <li key={index}>{ing}</li> 
     ));
-    return(
+    return (
       <div className="recipe-card">
-        <div className="recipe-img">
+        <div className="recipe-card-img">
           <img src={img} alt={title} />
         </div>
         <div className="recipe-card-content">
           <h3 className="recipe-title">{title}</h3>
-          <h4>ingredients:</h4>
+          <h4>Ingredients:</h4>
           <ul>
             {ingredients}
           </ul>
-          <h4>instrusction</h4>  
-          <p>{instrusction}</p>
+          <h4>Instructions:</h4>
+          <p>{instructions}</p>
+          <button type="button" onClick={() => onDelete(id)}>DELETE</button>
         </div>
-      </div>  
-    )
+      </div>
+    );
   }
 }
 
--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -1,15 +1,59 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
 import RecipeList from './RecipeList';
+import RecipeInput from './RecipeInput';
 import './RecipeApp.css';
 
 
 class RecipeApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipes: [
+        {
+          id: 0,
+          title: "Spaghetti",
+          instructions: "Open jar of Spaghetti sauce.  Bring to simmer.  Boil water.  Cook pasta until done.  Combine pasta and sauce",
+          ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
+          img: "spaghetti.jpg"
+        },
+        {
+          id: 1,
+          title: "Milkshake",
+          instructions: "Combine ice cream and milk.  Blend until creamy",
+          ingredients: ["2 Scoops Ice cream", "8 ounces milk"],
+          img: "milkshake.jpg"
+        },
+        {
+          id: 2,
+          title: "Avocado Toast",
+          instructions: "Toast bread.  Slice avocado and spread on bread.  Add salt, oil, and pepper to taste.",
+          ingredients: ["2 slices of bread", "1 avocado", "1 tablespoon olive oil", "1 pinch of salt", "pepper"],
+          img: "avocado_toast.jpg"
+        } 
+      ],
+      nextRcipedId: 3,
+    }
+
+    this.handleSave = this.handleSave.bind(this);
+  }
+  
+  handleSave(recipe) {
+    this.setState((prevState, props) => {
+      const newRecipe = {...recipe, id: this.state.nextRcipedId}
+      return {
+        nextRcipedId: prevState.nextRcipedId + 1,
+        recipes:  [...this.state.recipes, newRecipe]
+      }
+    });
+ }
+
   render() {
     return (
       <div className="App">
         <Navbar />
-        <RecipeList />
+        <RecipeInput onSave={this.handleSave}/>
+        <RecipeList recipes={this.state.recipes}/>
       </div>
     );
   }
