f3526ecf024ec18e05f7a1d96de399d83308ce83
Clifford <deeharvey06@yahoo.com>
2018-02-13 22:23:18 -0800

    add redux

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -1,71 +1,52 @@
 import React, { Component } from 'react';
-import Navbar from './Navbar';
-import RecipeList from './RecipeList';
-import RecipeInput from './RecipeInput';
+import {connect} from 'react-redux';
+import Navbar from './component/Navbar';
+import RecipeList from './component/RecipeList';
+import RecipeInput from './component/RecipeInput';
 import './RecipeApp.css';
 
-
 class RecipeApp extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      recipes: [
-        {
-          id: 0,
-          title: "Spaghetti",
-          instructions: "Open jar of Spaghetti sauce.  Bring to simmer.  Boil water.  Cook pasta until done.  Combine pasta and sauce",
-          ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
-          img: "spaghetti.jpg"
-        },
-        {
-          id: 1,
-          title: "Milkshake",
-          instructions: "Combine ice cream and milk.  Blend until creamy",
-          ingredients: ["2 Scoops Ice cream", "8 ounces milk"],
-          img: "milkshake.jpg"
-        },
-        {
-          id: 2,
-          title: "Avocado Toast",
-          instructions: "Toast bread.  Slice avocado and spread on bread.  Add salt, oil, and pepper to taste.",
-          ingredients: ["2 slices of bread", "1 avocado", "1 tablespoon olive oil", "1 pinch of salt", "pepper"],
-          img: "avocado_toast.jpg"
-        } 
-      ],
-      nextRcipedId: 3,
-      showform: false
-    }
 
-    this.handleSave = this.handleSave.bind(this);
-    this.onDelete = this.onDelete.bind(this);
-  }
-  
-  handleSave(recipe) {
-    this.setState((prevState, props) => {
-      const newRecipe = {...recipe, id: this.state.nextRcipedId}
-      return {
-        nextRcipedId: prevState.nextRcipedId + 1,
-        recipes:  [...this.state.recipes, newRecipe],
-        showform: false
-      }
-    });
-  }
-
-  onDelete(id) {
-    const recipes = this.state.recipes.filter(r => r.id !== id);
-    this.setState({recipes})
-  }
-  
   render() {
-    const {showform} = this.state;
+    const {shouldShowForm, recipes, saveRecipe, deleteRecipe, showForm, hideForm} = this.props;
     return (
       <div className="App">
-        <Navbar onNewRecipe={() => this.setState({showform: true})}/>
-        {showform ? <RecipeInput onSave={this.handleSave} onClose={() => this.setState({showform: false})}/> : null}  
-        <RecipeList onDelete={this.onDelete} recipes={this.state.recipes}/>
+        <Navbar onNewRecipe={showForm}/>
+        {shouldShowForm ? <RecipeInput onSave={saveRecipe} onClose={hideForm}/> : null}  
+        <RecipeList onDelete={deleteRecipe} recipes={recipes}/>
       </div>
     );
   }
 }
 
-export default RecipeApp;
+const mapStateToProps = state => ({
+  recipes: state.recipes,
+  shouldShowForm: state.shouldShowForm
+});
+
+const mapDispatchProps = dispatch => ({
+  saveRecipe(recipe) {
+    dispatch({
+      type: "SAVE_RECIPE",
+      recipe
+    })
+  },
+  deleteRecipe(recipeId) {
+    dispatch({
+      type: "DELETE_RECIPE",
+      recipeId
+    })
+  }, 
+  showForm() {
+    dispatch({
+      type: "SHOW_FORM",
+    })
+  }, 
+  hideForm() {
+    dispatch({
+      type: "HIDE_FORM",
+    })
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchProps)(RecipeApp);
